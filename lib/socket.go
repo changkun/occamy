45b62416659fc8974dd350e93f17ed3233659900
Ou Changkun <hi@changkun.us>
2019-04-30 17:15:52 +0200

    cgo: fix cgo cannot access existed c struct fields (#8)

--- lib/socket.go
+++ lib/socket.go
@@ -7,6 +7,7 @@ package lib
 /*
 #cgo LDFLAGS: -L/usr/local/lib -lguac
 #include "../guacamole/src/libguac/guacamole/socket.h"
+#include "../guacamole/src/libguac/guacamole/client.h"
 */
 import "C"
 import (
