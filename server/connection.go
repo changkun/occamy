45550a63d10d0d6043374db6bacc0ceed00734e6
Changkun Ou <hi@changkun.us>
2019-11-05 13:17:07 +0100

    server: allow pprof in debug mode
    This PR enables pprof for the occamy server.
    
    Fixes #16

--- server/connection.go
+++ server/connection.go
@@ -8,6 +8,7 @@ import (
 	"context"
 	"fmt"
 	"net/http"
+	"net/http/pprof"
 	"os"
 	"os/signal"
 	"sync"
@@ -132,7 +133,48 @@ func (p *proxy) setupRouter() (r *gin.Engine) {
 		auth.Use(jwtm.MiddlewareFunc())
 		auth.GET("", p.serveWS)
 	}
+	if gin.Mode() == gin.DebugMode {
+		profile(r)
+	}
 	return
+}
+
+// profile the standard HandlerFuncs from the net/http/pprof package with
+// the provided gin.Engine. prefixOptions is a optional. If not prefixOptions,
+// the default path prefix is used, otherwise first prefixOptions will be path prefix.
+//
+// Basic Usage:
+//
+// - use the pprof tool to look at the heap profile:
+//   go tool pprof http://0.0.0.0:5636/debug/pprof/heap
+// - look at a 30-second CPU profile:
+//   go tool pprof http://0.0.0.0:5636/debug/pprof/profile
+// - look at the goroutine blocking profile, after calling runtime.SetBlockProfileRate:
+//   go tool pprof http://0.0.0.0:5636/debug/pprof/block
+// - collect a 5-second execution trace:
+//   wget http://0.0.0.0:5636/debug/pprof/trace?seconds=5
+//
+func profile(r *gin.Engine) {
+	pprofHandler := func(h http.HandlerFunc) gin.HandlerFunc {
+		handler := http.HandlerFunc(h)
+		return func(c *gin.Context) {
+			handler.ServeHTTP(c.Writer, c.Request)
+		}
+	}
+	prefixRouter := r.Group("/debug/pprof")
+	{
+		prefixRouter.GET("/", pprofHandler(pprof.Index))
+		prefixRouter.GET("/cmdline", pprofHandler(pprof.Cmdline))
+		prefixRouter.GET("/profile", pprofHandler(pprof.Profile))
+		prefixRouter.POST("/symbol", pprofHandler(pprof.Symbol))
+		prefixRouter.GET("/symbol", pprofHandler(pprof.Symbol))
+		prefixRouter.GET("/trace", pprofHandler(pprof.Trace))
+		prefixRouter.GET("/block", pprofHandler(pprof.Handler("block").ServeHTTP))
+		prefixRouter.GET("/goroutine", pprofHandler(pprof.Handler("goroutine").ServeHTTP))
+		prefixRouter.GET("/heap", pprofHandler(pprof.Handler("heap").ServeHTTP))
+		prefixRouter.GET("/mutex", pprofHandler(pprof.Handler("mutex").ServeHTTP))
+		prefixRouter.GET("/threadcreate", pprofHandler(pprof.Handler("threadcreate").ServeHTTP))
+	}
 }
 
 func (p *proxy) serveWS(c *gin.Context) {
