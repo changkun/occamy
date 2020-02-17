e2907ae22f2bd54298e7229f1af0f7c159688cad
Changkun Ou <hi@changkun.us>
2020-02-17 17:34:34 +0100

    plugins: integrate settings to client

--- plugins/vnc/client.go
+++ plugins/vnc/client.go
@@ -4,26 +4,86 @@
 
 package vnc
 
-// VNC ...
-type VNC struct {
+const (
+	// FrameDuration is the maximum duration of a frame in milliseconds.
+	FrameDuration = 40
+	// FrameTimeout is the amount of time to allow per message read
+	// within a frame, in milliseconds. If the server is silent for at
+	// least this amount of time, the frame will be considered finished.
+	FrameTimeout = 0
+	// FrameStartTimeout is the amount of time to wait for a new message
+	// from the VNC server when beginning a new frame. This value must
+	// be kept reasonably small such that a slow VNC server will not
+	// prevent external events from being handled (such as the stop
+	// signal from guac_client_stop()), but large enough that the
+	// message handling loop does not eat up CPU spinning.
+	FrameStartTimeout = 1000000
+	// ConnectInterval is the number of milliseconds to wait between
+	// connection attempts.
+	ConnectInterval = 1000
+	// ClipboardMaxLength is the maximum number of bytes to allow within
+	// the clipboard.
+	ClipboardMaxLength = 262144
+	// ClientKey which can be used with the rfbClientGetClientData
+	// function to return the associated guac_client.
+	ClientKey = "GUAC_VNC"
+)
+
+// client plugin arguments
+var clientArgs = []string{
+	"hostname",
+	"port",
+	"read-only",
+	"encodings",
+	"password",
+	"swap-red-blue",
+	"color-depth",
+	"cursor",
+	"autoretry",
+	"clipboard-encoding",
+	"dest-host",
+	"dest-port",
+	"reverse-connect",
+	"listen-timeout",
+}
+
+// Settings ...
+type Settings struct {
+	hostname          string
+	port              int
+	password          string
+	encoding          string
+	swapRedBlue       bool
+	colorDepth        int
+	readOnly          bool
+	retries           int
+	clipboardEncoding string
+}
+
+// Parse ...
+func (s *Settings) Parse() {
+}
+
+// Client ...
+type Client struct {
 }
 
 // NewClient ...
-func NewClient() *VNC {
-	return &VNC{}
+func NewClient() *Client {
+	return &Client{}
 }
 
 // Join ...
-func (vnc *VNC) Join() {
+func (c *Client) Join() {
 
 }
 
 // Leave ...
-func (vnc *VNC) Leave() {
+func (c *Client) Leave() {
 
 }
 
 // Free ...
-func (vnc *VNC) Free() {
+func (c *Client) Free() {
 
 }
