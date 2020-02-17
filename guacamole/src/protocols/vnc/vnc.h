741954f4badcde455f3df7731760b1f2a2c3dac1
Changkun Ou <hi@changkun.us>
2020-02-17 17:34:20 +0100

    guacamole: remove more vnc declaration

--- guacamole/src/protocols/vnc/vnc.h
+++ guacamole/src/protocols/vnc/vnc.h
@@ -122,73 +122,6 @@ typedef struct guac_vnc_settings {
 } guac_vnc_settings;
 
 /**
- * Parses all given args, storing them in a newly-allocated settings object. If
- * the args fail to parse, NULL is returned.
- *
- * @param user
- *     The user who submitted the given arguments while joining the
- *     connection.
- *
- * @param argc
- *     The number of arguments within the argv array.
- *
- * @param argv
- *     The values of all arguments provided by the user.
- *
- * @return
- *     A newly-allocated settings object which must be freed with
- *     guac_vnc_settings_free() when no longer needed. If the arguments fail
- *     to parse, NULL is returned.
- */
-guac_vnc_settings* guac_vnc_parse_args(guac_user* user,
-        int argc, const char** argv);
-
-/**
- * Frees the given guac_vnc_settings object, having been previously allocated
- * via guac_vnc_parse_args().
- *
- * @param settings
- *     The settings object to free.
- */
-void guac_vnc_settings_free(guac_vnc_settings* settings);
-
-/**
- * NULL-terminated array of accepted client args.
- */
-extern const char* GUAC_VNC_CLIENT_ARGS[];
-
-/**
- * The maximum duration of a frame in milliseconds.
- */
-#define GUAC_VNC_FRAME_DURATION 40
-
-/**
- * The amount of time to allow per message read within a frame, in
- * milliseconds. If the server is silent for at least this amount of time, the
- * frame will be considered finished.
- */
-#define GUAC_VNC_FRAME_TIMEOUT 0
-
-/**
- * The amount of time to wait for a new message from the VNC server when
- * beginning a new frame. This value must be kept reasonably small such that
- * a slow VNC server will not prevent external events from being handled (such
- * as the stop signal from guac_client_stop()), but large enough that the
- * message handling loop does not eat up CPU spinning.
- */
-#define GUAC_VNC_FRAME_START_TIMEOUT 1000000
-
-/**
- * The number of milliseconds to wait between connection attempts.
- */
-#define GUAC_VNC_CONNECT_INTERVAL 1000
-
-/**
- * The maximum number of bytes to allow within the clipboard.
- */
-#define GUAC_VNC_CLIPBOARD_MAX_LENGTH 262144
-
-/**
  * VNC-specific client data.
  */
 typedef struct guac_vnc_client {
@@ -243,21 +176,6 @@ typedef struct guac_vnc_client {
 } guac_vnc_client;
 
 /**
- * Allocates a new rfbClient instance given the parameters stored within the
- * client, returning NULL on failure.
- *
- * @param client
- *     The guac_client associated with the settings of the desired VNC
- *     connection.
- *
- * @return
- *     A new rfbClient instance allocated and connected according to the
- *     parameters stored within the given client, or NULL if connecting to the
- *     VNC server fails.
- */
-rfbClient* guac_vnc_get_client(guac_client* client);
-
-/**
  * VNC client thread. This thread initiates the VNC connection and ultimately
  * runs throughout the duration of the client, existing as a single instance,
  * shared by all users.
@@ -269,12 +187,6 @@ rfbClient* guac_vnc_get_client(guac_client* client);
  *     Always NULL.
  */
 void* guac_vnc_client_thread(void* data);
-
-/**
- * Key which can be used with the rfbClientGetClientData function to return
- * the associated guac_client.
- */
-extern char* GUAC_VNC_CLIENT_KEY;
 
 #endif
 
